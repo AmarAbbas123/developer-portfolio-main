0f5d318c8670a51ee57dc2e279fa31647543e536
Amar Abbas <113879608+AmarAbbas123@users.noreply.github.com>
2026-07-17 09:59:30 +0500

    Refactor skillsData for better organization
    Removed several skills and added comments for organization.

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,107 +1,24 @@
 export const skillsData = [
+  // Front-End Core & Frameworks
   'HTML',
-  'CSS',
-  'Javascript',
+  'JS',
   'Typescript',
   'React',
-  'Laravel'
-  
+
+  // Styling & Design
+  'CSS',
   'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'WordPress',
-  'Git',
- 
   'Bootstrap',
-
   'MaterialUI',
-  
-]
 
-// Choose your skills from below. Make sure it's in the same format and spelled correctly.
+  // Back-End & CMS
+  'Laravel',
+  'Wordpress',
 
+  // Databases
+  'MySQL',
+  'MongoDB',
 
-
-// AVAILABLE SKILLS
-
-/* 
-  HTML
-  CSS
-  JS 
-  React
-  Next JS
-  Nuxt JS
-  Node JS
-  Vue
-  Angular
-  Docker
-  Photoshop
-  Illustrator
-  Svelte
-  GCP
-  Azure
-  Fastify
-  Haxe
-  Ionic
-  Markdown
-  Microsoft Office
-  Picsart
-  Sketch
-  Unity
-  WolframAlpha
-  Adobe XD
-  After Effects
-  Bootstrap
-  Bulma
-  CapacitorJs
-  Coffeescript
-  MemSQL
-  C
-  C++
-  C#
-  Python
-  Java
-  Julia
-  Matlab
-  Swift
-  Ruby
-  Kotlin
-  Go
-  PHP
-  Flutter
-  Dart
-  Typescript
-  Swift
-  Git
-  Figma
-  Canva
-  Ubuntu
-  Bootstrap
-  MongoDB
-  Tailwind
-  ViteJS
-  VuetifyJS
-  MySQL
-  PostgreSQL
-  Prisma
-  AWS
-  Firebase
-  Blender
-  Premiere Pro
-  Adobe Audition
-  Deno
-  Django
-  Gimp
-  Graphql
-  Lightroom
-  MaterialUI
-  Nginx
-  Numpy
-  OpenCV
-  Pytorch
-  Selenium
-  Strapi
-  Tensorflow
-  Webex
-  Wordpress
-*/
+  // Version Control & Tools
+  'Git'
+];
